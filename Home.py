53148eb3f003b8356b1a607d6d0e7f331f83b80c
Jannik Harm <64127899+jannik-reaZn@users.noreply.github.com>
2023-01-07 23:18:02 +0100

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,25 +1,28 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(layout='wide')
+# page settings
+st.set_page_config(page_title='CS:GO Utilities', layout='wide')
+
 st.title('CSGO Utilities')
+
 st.markdown(
-  'CSGO Nades is a website that collects nades for Counter-Strike Global Offensive. You can browse smokes, flashbangs, molotovs or he-grenades.  \n'
-  'Important: All utilities are for **64-Tick** server!')
+  'CSGO Nades is a website that collects nades for Counter-Strike Global Offensive. You can browse smokes, flashbangs, molotovs or he-grenades. All utilities are for **64-Tick** servers')
 
 # key bindings
 st.header('Jump Throw Bind')
-st.markdown(
+st.write(
   'alias "+jumpthrow" "+jump;-attack";  \n'
   'alias "-jumpthrow" "-jump";  \n'
   'bind "<key>" "+jumpthrow"')
 
 st.header('Run Throw Bind')
-st.markdown(
+st.write(
   'alias "+forwardjumpthrow" "+forward; +jump;-attack;-attack2";  \n'
   'alias "-forwardjumpthrow" "-jump; -forward"  \n'
   'bind "<key>" "+forwardjumpthrow"')
 
 st.header('Toogle Crosshairsize')
-st.markdown(f'<p style="background-color:#cccccc; border-radius:2%;">bind "key" "toggle cl_crosshairsize 1000 <your cl_crosshairsize>"</p>', unsafe_allow_html=True)
+st.write('bind "<key>" "toggle cl_crosshairsize 1000 <your cl_crosshairsize>')
+st.text('bind "<key>" "toggle cl_crosshairsize 1000 <your cl_crosshairsize>')
 
