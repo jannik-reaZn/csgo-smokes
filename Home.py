59cc06f8fd09564084ccd0220aafa1b5c077d961
Jannik Harm <64127899+jannik-reaZn@users.noreply.github.com>
2023-01-11 21:41:56 +0100

    Add files via upload

--- Home.py
+++ Home.py
@@ -3,26 +3,27 @@ from PIL import Image
 
 # page settings
 st.set_page_config(page_title='CS:GO Utilities', layout='wide')
-
 st.title('CSGO Utilities')
 
+# main text
 st.markdown(
-  'CSGO Nades is a website that collects nades for Counter-Strike Global Offensive. You can browse smokes, flashbangs, molotovs or he-grenades. All utilities are for **64-Tick** servers')
+  'CSGO Nades is a website that collects nades for Counter-Strike Global Offensive. You can browse smokes, flashbangs, molotovs or he-grenades. All utilities are for **64-Tick** servers.')
 
-# key bindings
+# jump throw bind
 st.header('Jump Throw Bind')
-st.write(
+st.text(
   'alias "+jumpthrow" "+jump;-attack";  \n'
   'alias "-jumpthrow" "-jump";  \n'
   'bind "<key>" "+jumpthrow"')
 
+# run throw bind
 st.header('Run Throw Bind')
-st.write(
+st.text(
   'alias "+forwardjumpthrow" "+forward; +jump;-attack;-attack2";  \n'
   'alias "-forwardjumpthrow" "-jump; -forward"  \n'
   'bind "<key>" "+forwardjumpthrow"')
 
+# toggle crosshair bind
 st.header('Toogle Crosshairsize')
-st.write('bind "<key>" "toggle cl_crosshairsize 1000 <your cl_crosshairsize>')
 st.text('bind "<key>" "toggle cl_crosshairsize 1000 <your cl_crosshairsize>')
 
--- pages/Mirage.py
+++ pages/Mirage.py
@@ -9,7 +9,7 @@ st.title('Mirage Nades')
 images_dir = 'images/mirage/'
 videos_dir = 'videos/mirage/'
 
-def openImageAndDisplay(image, dir=images_dir, width=350):
+def openImageAndDisplay(image, dir=images_dir, width=300):
   """  Opens an image and displays it in the streamlit app. """
   image = Image.open(images_dir + image)
   st.image(image, width=width)
@@ -33,67 +33,155 @@ if utility == 'Smoke':
   col1, col2, col3, col4 = st.columns(4)
   with st.container():
     with col1:
-      openImageAndDisplay('stairs.jpg')
+      openImageAndDisplay('/smokes/stairs.jpg')
     with col2:
-      openVideoAndDisplay('stairs.mp4')
+      openVideoAndDisplay('/smokes/stairs.mp4')
   displayHorizontalLine()
-  ###########################################################################
+
   st.markdown('##### Jungle')
   col1, col2, col3, col4 = st.columns(4)
   with st.container():
     with col1:
-      openImageAndDisplay('jungle.jpg')
+      openImageAndDisplay('/smokes/jungle.jpg')
     with col2:
-      openVideoAndDisplay('jungle.mp4')
+      openVideoAndDisplay('/smokes/jungle.mp4')
   displayHorizontalLine()
-  ###########################################################################
+
+  st.markdown('##### Jungle Deep')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/jungle-deep.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/jungle-deep-from-tetris.mp4')
+  displayHorizontalLine()
+
+
+  st.markdown('##### CT-Spawn')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/ct-spawn.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/ct-spawn.mp4')
+    with col3:
+      openVideoAndDisplay('/smokes/ct-spawn-from-tetris.mp4')
+    with col4:
+      openVideoAndDisplay('/smokes/ct-spawn-from-top-mid.mp4')
+  displayHorizontalLine()
+
+
   st.markdown('##### Slope')
   col1, col2, col3, col4 = st.columns(4)
   with st.container():
     with col1:
-      openImageAndDisplay('slope.jpg')
+      openImageAndDisplay('/smokes/slope.jpg')
     with col2:
-      openVideoAndDisplay('slope.mp4')
+      openVideoAndDisplay('/smokes/slope.mp4')
   displayHorizontalLine()
-  ###########################################################################
-  st.markdown('##### Connector Oneway')
+
+  st.markdown('##### Connector top')
   col1, col2, col3, col4 = st.columns(4)
   with st.container():
     with col1:
-      openImageAndDisplay('connector-oneway.jpg')
+      openImageAndDisplay('/smokes/connector-top.jpg')
     with col2:
-      openVideoAndDisplay('connector-oneway.mp4')
-    displayHorizontalLine()
-    ###########################################################################
-    st.markdown('##### Deep Apps')
+      openVideoAndDisplay('/smokes/connector-top-from-tetris.mp4')
+    with col3:
+      openVideoAndDisplay('/smokes/connector-top-from-top-mid.mp4')
+    with col4:
+      openVideoAndDisplay('/smokes/connector-top-from-side-alley-2.mp4')
     col1, col2, col3, col4 = st.columns(4)
-    with st.container():
-      with col1:
-        openImageAndDisplay('apps-deep.jpg')
-      with col2:
-        openVideoAndDisplay('apps-from-side.mp4')
-      with col3:
-        openVideoAndDisplay('apps-from-truck.mp4')
+    with col1:
+      openVideoAndDisplay('/smokes/connector-top-from-side-alley-1.mp4')
+    with col2:
+      openVideoAndDisplay('/smokes/connector-top-from-ct-spawn.mp4')
     displayHorizontalLine()
-    ###########################################################################
-    st.markdown('##### Arch Left')
-    col1, col2, col3, col4 = st.columns(4)
-    with st.container():
-      with col1:
-        openImageAndDisplay('arch-left.jpg')
-      with col2:
-        openVideoAndDisplay('arch-left.mp4')
+
+  st.markdown('##### Connector Top Oneway')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/connector-oneway.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/connector-oneway.mp4')
     displayHorizontalLine()
-    ###########################################################################
-    st.markdown('##### Arch Right')
-    col1, col2, col3, col4 = st.columns(4)
-    with st.container():
-      with col1:
-        openImageAndDisplay('arch-right.jpg')
-      with col2:
-        openVideoAndDisplay('arch-right.mp4')
-      with col3:
-        openVideoAndDisplay('arch-right-jumpthrow.mp4')
+
+  st.markdown('##### Connector Bottom')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/connector-bottom.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/connector-bottom-from-short.mp4')
+  displayHorizontalLine()
+ 
+  st.markdown('##### Apps')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/apps.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/apps-from-trash.mp4')
+  displayHorizontalLine()
+
+
+  st.markdown('##### Deep Apps')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/apps-deep.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/apps-from-side.mp4')
+    with col3:
+      openVideoAndDisplay('/smokes/apps-from-truck.mp4')
+  displayHorizontalLine()
+
+  st.markdown('##### Arch Left')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/arch-left.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/arch-left.mp4')
+  displayHorizontalLine()
+
+  st.markdown('##### Arch Right')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/arch-right.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/arch-right.mp4')
+    with col3:
+      openVideoAndDisplay('/smokes/arch-right-jumpthrow.mp4')
+
+  st.markdown('##### Short')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/short.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/short.mp4')
+
+  st.markdown('##### Window')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/smokes/window.jpg')
+    with col2:
+      openVideoAndDisplay('/smokes/window-from-top-mid.mp4')
+    with col3:
+      openVideoAndDisplay('/smokes/window-from-spawn-1.mp4')
+    with col4:
+      openVideoAndDisplay('/smokes/window-from-spawn-2.mp4')
+
+  st.markdown('##### Mid Top')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col2:
+      openVideoAndDisplay('/smokes/mid-top.mp4')
+
 
 ###############################################################################
 if utility == 'Molotov':
@@ -101,9 +189,17 @@ if utility == 'Molotov':
   col1, col2, col3, col4 = st.columns(4)
   with st.container():
     with col1:
-      openImageAndDisplay('sandwitch.jpg')
+      openImageAndDisplay('/molotovs/sandwitch.jpg')
     with col2:
-      openVideoAndDisplay('sandwitch.mp4')
+      openVideoAndDisplay('/molotovs/sandwitch.mp4')
+
+  st.markdown('##### Triple')
+  col1, col2, col3, col4 = st.columns(4)
+  with st.container():
+    with col1:
+      openImageAndDisplay('/molotovs/triple.jpg')
+    with col2:
+      openVideoAndDisplay('/molotovs/triple-from-tetris.mp4')
 
 ###############################################################################
 if utility == 'HE-Grenade':
